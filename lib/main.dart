3817cf5f0d993e58b9efbfa2fe08a5a54ce353dc
Huiwon <71807924+hu1won@users.noreply.github.com>
2022-11-03 15:44:23 +0900

    flutter init

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,6 @@ class _MyAppState extends State<MyApp> {
   @override
   void initState() {
     super.initState();
-    idgetsBinding.instance.addPostFrameCallback((_) => yourFunction(context));
   }
 
   @override
