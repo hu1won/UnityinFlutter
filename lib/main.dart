f4c7ce27521312d1f13ae9e7ca4a66445e32b64a
Huiwon <71807924+hu1won@users.noreply.github.com>
2022-11-03 15:42:58 +0900

    save

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,7 @@ class _MyAppState extends State<MyApp> {
   @override
   void initState() {
     super.initState();
+    idgetsBinding.instance.addPostFrameCallback((_) => yourFunction(context));
   }
 
   @override
